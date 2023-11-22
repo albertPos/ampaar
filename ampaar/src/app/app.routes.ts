eaa2316406d26509f7f0ee3dffc7afd6b5dd659c
albertPos <albert.poselskii@gmail.com>
2023-11-22 15:46:51 +0900

    2

--- ampaar/src/app/app.routes.ts
+++ ampaar/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './Components/home-page/home-page.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: '', component: AppComponent,
+    children: [
+      { path: '', component: HomePageComponent},
+      { path: 'quest', loadChildren: () => import('./modules/lk/lk.module').then(m => m.LkModule)},
+    ]},
+  
+  { path: '**', redirectTo: '' }
+
+
+];
---
+++ ampaar/src/app/modules/lk/lk/lk.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-lk',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './lk.component.html',
+  styleUrl: './lk.component.css'
+})
+export class LkComponent {
+
+}
